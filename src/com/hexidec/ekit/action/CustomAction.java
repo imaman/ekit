764fafe640fe0830ce9e4fc3285540701680d270
Itay Maman <itay.maman@gmail.com>
2011-02-09 21:15:28 +0200

    snapshot at the end of SCIL6

--- src/com/hexidec/ekit/action/CustomAction.java
+++ src/com/hexidec/ekit/action/CustomAction.java
@@ -57,106 +57,147 @@ public class CustomAction extends StyledEditorKit.StyledTextAction {
 
   public void actionPerformed(ActionEvent ae) {
     mutator = parentEkit.getMutator();
-    if (this.isEnabled()) {
-      Hashtable<String, String> htmlAttribs2 = new Hashtable<String, String>();
-      MyJTextPane parentTextPane = parentEkit.getTextPane();
-      String selText = parentTextPane.getSelectedText();
-      int textLength = -1;
-      if (selText != null) {
-        textLength = selText.length();
-      }
-      if (selText == null || textLength < 1) {
-        parentEkit.showInfoDialog(Translatrix.getTranslationString("Error"),
-          true, Translatrix.getTranslationString("ErrorNoTextSelected"),
-          SimpleInfoDialog.ERROR);
-      } else {
-        int caretOffset = parentTextPane.getSelectionStart();
-        int internalTextLength = selText.length();
-        String currentAnchor = "";
-        // Somewhat ham-fisted code to obtain the first HREF in the selected
-        // text,
-        // which (if found) is passed to the URL HREF request dialog.
-        if (htmlTag.toString().equals(HTML.Tag.A.toString())) {
-          SimpleAttributeSet sasText = null;
-          for (int i = caretOffset; i < caretOffset + internalTextLength; i++) {
-            parentTextPane.select(i, i + 1);
-            sasText = parentTextPane.newSimpleAttributeSet();
-            Enumeration<?> attribEntries1 = sasText.getAttributeNames();
-            while (attribEntries1.hasMoreElements() && currentAnchor.equals("")) {
-              Object entryKey = attribEntries1.nextElement();
-              Object entryValue = sasText.getAttribute(entryKey);
-              if (entryKey.toString().equals(HTML.Tag.A.toString())) {
-                if (entryValue instanceof SimpleAttributeSet) {
-                  Enumeration<?> subAttributes = ((SimpleAttributeSet) entryValue)
-                    .getAttributeNames();
-                  while (subAttributes.hasMoreElements()
-                    && currentAnchor.equals("")) {
-                    Object subKey = subAttributes.nextElement();
-                    if (subKey.toString().toLowerCase().equals("href")) {
-                      currentAnchor = ((SimpleAttributeSet) entryValue)
-                        .getAttribute(subKey).toString();
-                      break;
-                    }
-                  }
-                }
-              }
-            }
-            if (!currentAnchor.equals("")) {
-              break;
-            }
-          }
-        }
-        parentTextPane.select(caretOffset, caretOffset + internalTextLength);
-        SimpleAttributeSet sasTag = new SimpleAttributeSet();
-        SimpleAttributeSet sasAttr = new SimpleAttributeSet();
-        if (htmlTag.toString().equals(HTML.Tag.A.toString())) {
-          if (!htmlAttribs.containsKey("href")) {
-            MyUserInputAnchorDialog uidInput = parentEkit
-              .newUserInputAnchorDialog(parentEkit,
-                Translatrix.getTranslationString("AnchorDialogTitle"), true,
-                currentAnchor);
-            String newAnchor = uidInput.getInputText();
-            uidInput.dispose();
-            if (newAnchor != null) {
-              htmlAttribs2.put("href", newAnchor);
-            } else {
-              parentEkit.repaint();
-              return;
-            }
-          }
-        } else if (htmlTag.toString().equals(HTML.Tag.FONT.toString())) {
-          if (htmlAttribs.containsKey("color")) {
-            Color color = parentEkit.chooseColor(
-              Translatrix.getTranslationString("CustomColorDialog"),
-              Color.black);
-            if (color != null) {
-              parentEkit.setForegroundColor(color, ae);
-            }
-          }
-        }
-        if (htmlAttribs2.size() > 0) {
-          Enumeration<String> attribEntries = htmlAttribs2.keys();
-          while (attribEntries.hasMoreElements()) {
-            Object entryKey = attribEntries.nextElement();
-            Object entryValue = htmlAttribs2.get(entryKey);
-            insertAttribute(sasAttr, entryKey, entryValue);
-          }
-          SimpleAttributeSet baseAttrs = parentEkit.getTextPane()
-            .newSimpleAttributeSet();
-          mutator.mutate(baseAttrs);
+    if (!this.isEnabled()) 
+      return;
+      
+    MyJTextPane parentTextPane = parentEkit.getTextPane();
+    String selText = parentTextPane.getSelectedText();
+    
+    if (selText == null || selText.length() < 1) {
+      parentEkit.showInfoDialog(Translatrix.getTranslationString("Error"),
+        true, Translatrix.getTranslationString("ErrorNoTextSelected"),
+        SimpleInfoDialog.ERROR);
+      
+      return;
+    } 
+    
+    something(ae, parentTextPane, selText);
+  }
 
-          Enumeration<?> attribEntriesOriginal = baseAttrs.getAttributeNames();
-          while (attribEntriesOriginal.hasMoreElements()) {
-            Object entryKey = attribEntriesOriginal.nextElement();
-            Object entryValue = baseAttrs.getAttribute(entryKey);
-            insertAttribute(sasAttr, entryKey, entryValue);
+  private void something(ActionEvent ae, MyJTextPane parentTextPane,
+    String selText) {
+    int caretOffset = parentTextPane.getSelectionStart();
+    int internalTextLength = selText.length();
+    // Somewhat ham-fisted code to obtain the first HREF in the selected
+    // text,
+    // which (if found) is passed to the URL HREF request dialog.
+    String currentAnchor = "";
+    if (htmlTag.toString().equals(HTML.Tag.A.toString())) {
+      currentAnchor = findCurrentAnchor(parentTextPane, caretOffset,
+        internalTextLength);
+    }
+
+    Hashtable<String, String> htmlAttribs2 = new Hashtable<String, String>();
+
+    parentTextPane.select(caretOffset, caretOffset + internalTextLength);
+    if (htmlTag.toString().equals(HTML.Tag.A.toString())) {
+      boolean shouldExit = something6(currentAnchor, htmlAttribs2);      
+      if(shouldExit)
+        return;
+      
+
+    } else if (htmlTag.toString().equals(HTML.Tag.FONT.toString())) {
+      something2(ae);
+    }
+    
+    if (htmlAttribs2.size() > 0) {
+      something3(parentTextPane, htmlAttribs2);
+    }
+    
+    parentTextPane.select(caretOffset, caretOffset + internalTextLength);
+    parentTextPane.requestFocus();
+  }
+
+  private String findCurrentAnchor(MyJTextPane parentTextPane, int caretOffset,
+    int internalTextLength) {
+    for (int i = caretOffset; i < caretOffset + internalTextLength; ++i) {
+      parentTextPane.select(i, i + 1);
+      SimpleAttributeSet sasText = parentTextPane.newSimpleAttributeSet();
+      Enumeration<?> attribEntries1 = sasText.getAttributeNames();
+      while (attribEntries1.hasMoreElements()) {
+        String result = findCurrentAnchor(sasText, attribEntries1);
+        if(!result.isEmpty())
+          return result;
+      }
+    }
+    return "";
+  }
+
+  private boolean something6(String currentAnchor,
+    Hashtable<String, String> htmlAttribs2) {
+    boolean shouldExit = false;
+    if (!htmlAttribs.containsKey("href")) {
+      MyUserInputAnchorDialog uidInput = parentEkit
+        .newUserInputAnchorDialog(parentEkit,
+          Translatrix.getTranslationString("AnchorDialogTitle"), true,
+          currentAnchor);
+      String newAnchor = uidInput.getInputText();
+      uidInput.dispose();
+      if (newAnchor != null) {
+        htmlAttribs2.put("href", newAnchor);
+      } else {
+        parentEkit.repaint();
+        shouldExit = true;
+      }
+    }
+    return shouldExit;
+  }
+
+  private String findCurrentAnchor(SimpleAttributeSet sasText, Enumeration<?> attribEntries1) {
+    String currentAnchor = "";
+    Object entryKey = attribEntries1.nextElement();
+    Object entryValue = sasText.getAttribute(entryKey);
+    if (entryKey.toString().equals(HTML.Tag.A.toString())) {
+      if (entryValue instanceof SimpleAttributeSet) {
+        Enumeration<?> subAttributes = ((SimpleAttributeSet) entryValue)
+          .getAttributeNames();
+        while (subAttributes.hasMoreElements()
+          && currentAnchor.equals("")) {
+          Object subKey = subAttributes.nextElement();
+          if (subKey.toString().toLowerCase().equals("href")) {
+            currentAnchor = ((SimpleAttributeSet) entryValue)
+              .getAttribute(subKey).toString();
+            break;
           }
-          sasTag.addAttribute(htmlTag, sasAttr);
-          parentTextPane.setCharacterAttributes(sasTag, false);
-          parentEkit.refreshOnUpdate();
         }
-        parentTextPane.select(caretOffset, caretOffset + internalTextLength);
-        parentTextPane.requestFocus();
+      }
+    }
+    return currentAnchor;
+  }
+
+  private void something3(MyJTextPane parentTextPane,
+    Hashtable<String, String> htmlAttribs2) {
+    SimpleAttributeSet sasTag = new SimpleAttributeSet();
+    SimpleAttributeSet sasAttr = new SimpleAttributeSet();
+
+    Enumeration<String> attribEntries = htmlAttribs2.keys();
+    while (attribEntries.hasMoreElements()) {
+      Object entryKey = attribEntries.nextElement();
+      Object entryValue = htmlAttribs2.get(entryKey);
+      insertAttribute(sasAttr, entryKey, entryValue);
+    }
+    SimpleAttributeSet baseAttrs = parentEkit.getTextPane()
+      .newSimpleAttributeSet();
+    mutator.mutate(baseAttrs);
+
+    Enumeration<?> attribEntriesOriginal = baseAttrs.getAttributeNames();
+    while (attribEntriesOriginal.hasMoreElements()) {
+      Object entryKey = attribEntriesOriginal.nextElement();
+      Object entryValue = baseAttrs.getAttribute(entryKey);
+      insertAttribute(sasAttr, entryKey, entryValue);
+    }
+    sasTag.addAttribute(htmlTag, sasAttr);
+    parentTextPane.setCharacterAttributes(sasTag, false);
+    parentEkit.refreshOnUpdate();
+  }
+
+  private void something2(ActionEvent ae) {
+    if (htmlAttribs.containsKey("color")) {
+      Color color = parentEkit.chooseColor(
+        Translatrix.getTranslationString("CustomColorDialog"),
+        Color.black);
+      if (color != null) {
+        parentEkit.setForegroundColor(color, ae);
       }
     }
   }
